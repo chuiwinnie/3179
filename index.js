5f1670924cc2f6ff36e4db8f176c91d25cdf9b2f
chuiwinnie <winniechui0106@gmail.com>
2024-10-15 17:34:35 +1100

    Added dropdown selection for industries

--- index.js
+++ index.js
@@ -14,74 +14,116 @@ d3.csv("./data/Billionaires_Data.csv")
 
       // Initialise industry in the map if not already present
       if (!industryMap[industry]) {
-        industryMap[industry] = { organizations: {}, total: 0 };
+        industryMap[industry] = { organisations: {}, total: 0 };
       }
 
       // Add organisation and its net worth
-      if (!industryMap[industry].organizations[organisation]) {
-        industryMap[industry].organizations[organisation] = 0;
+      if (!industryMap[industry].organisations[organisation]) {
+        industryMap[industry].organisations[organisation] = 0;
       }
-      industryMap[industry].organizations[organisation] += netWorth;
+      industryMap[industry].organisations[organisation] += netWorth;
       industryMap[industry].total += netWorth;
     });
 
-    // Building arrays for Plotly
+    // Populate the checkboxes with unique industries
+    const industryCheckboxes = d3.select("#industry-checkboxes");
     Object.keys(industryMap).forEach((industry) => {
-      parents.push(""); // Industry has no parent
-      labels.push(industry);
-      values.push(industryMap[industry].total);
-
-      Object.keys(industryMap[industry].organizations).forEach(
-        (organization) => {
-          if (organization) {
-            // If billionaire has an organisation
-            parents.push(industry); // Organisation's parent is the industry
-            labels.push(organization);
-            values.push(industryMap[industry].organizations[organization]);
-          }
-        }
-      );
+      industryCheckboxes.append("div").attr("class", "form-check").html(`
+          <input class="form-check-input" type="checkbox" value="${industry}" id="${industry}">
+          <label class="form-check-label" for="${industry}">${industry}</label>
+        `);
     });
 
-    // Format number with thousand separators
-    function formatNumber(number) {
-      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    // Function to draw the sunburst chart based on selected industries
+    function drawSunburst(selectedIndustries) {
+      parents.length = 0;
+      labels.length = 0;
+      values.length = 0;
+
+      Object.keys(industryMap).forEach((industry) => {
+        if (
+          selectedIndustries.includes("all") ||
+          selectedIndustries.includes(industry)
+        ) {
+          parents.push("");
+          labels.push(industry);
+          values.push(industryMap[industry].total);
+
+          Object.keys(industryMap[industry].organisations).forEach(
+            (organisation) => {
+              if (organisation) {
+                parents.push(industry);
+                labels.push(organisation);
+                values.push(industryMap[industry].organisations[organisation]);
+              }
+            }
+          );
+        }
+      });
+
+      const hovertext = labels.map((label, index) => {
+        const valueInBillions = (values[index] / 1e9).toFixed(0);
+        const formattedValue = valueInBillions
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ","); // format with thousand separators
+        if (parents[index] !== "") {
+          return `Organisation: <b>${label}</b><br>Net Worth: <b>$${formattedValue}B</b>`;
+        } else {
+          return `Industry: <b>${label}</b><br>Total Net Worth: <b>$${formattedValue}B</b>`;
+        }
+      });
+
+      const trace = {
+        type: "sunburst",
+        labels: labels,
+        parents: parents,
+        values: values,
+        branchvalues: "total",
+        hoverinfo: "text",
+        hovertext: hovertext,
+        insidetextorientation: "radial",
+      };
+
+      const data = [trace];
+
+      const layout = {
+        title:
+          "Distribution of Billionaires' Wealth by Industry and Organisation",
+        margin: { l: 80, r: 0, b: 0, t: 100 },
+        width: 800,
+        height: 800,
+      };
+
+      Plotly.newPlot("sunburst", data, layout);
     }
 
-    // Create arrays for hover text
-    const hovertext = labels.map((label, index) => {
-      const valueInBillions = (values[index] / 1e9).toFixed(0); // Convert to billions
-      const formattedValue = formatNumber(valueInBillions);
+    // Initial draw of the sunburst chart
+    drawSunburst(["all"]);
 
-      if (parents[index] !== "") {
-        return `Organisation: <b>${label}</b><br>Net Worth: <b>$${formattedValue}B</b>`;
-      } else {
-        return `Industry: <b>${label}</b><br>Total Net Worth: <b>$${formattedValue}B</b>`;
+    // Update the chart when a checkbox changes
+    d3.selectAll("input[type='checkbox']").on("change", function () {
+      Array.from(d3.selectAll("input[type='checkbox']:checked").nodes()).map(
+        (option) => option.value
+      );
+
+      // If "All" is checked, clear all other selections
+      if (this.value === "all" && this.checked) {
+        d3.selectAll("input[type='checkbox']")
+          .filter(function () {
+            return this.value !== "all";
+          })
+          .property("checked", false); // Uncheck all options except "All"
+      } else if (this.value !== "all" && this.checked) {
+        // If any other industry is checked, uncheck "All"
+        d3.select("#checkAll").property("checked", false);
       }
+
+      // Update the sunburst chart based on selected options
+      const updatedSelectedOptions = Array.from(
+        d3.selectAll("input[type='checkbox']:checked").nodes()
+      ).map((option) => option.value);
+
+      drawSunburst(updatedSelectedOptions);
     });
-
-    // Create the sunburst chart
-    const trace = {
-      type: "sunburst",
-      labels: labels,
-      parents: parents,
-      values: values,
-      branchvalues: "total",
-      hoverinfo: "text", // Show custom hover text
-      hovertext: hovertext, // Set custom hover text
-      insidetextorientation: "radial",
-    };
-
-    const data = [trace];
-
-    const layout = {
-      title:
-        "Distribution of Billionaires' Wealth by Industry and Organisation",
-      margin: { l: 80, r: 0, b: 0, t: 100 },
-      width: 800,
-      height: 800,
-    };
-
-    Plotly.newPlot("sunburst", data, layout);
   })
   .catch((error) => console.error("Error loading or processing data:", error));
